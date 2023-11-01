1850243f26b04b5de186f424ae045b0136794c59
Dexter <112994984+dexterroberts@users.noreply.github.com>
2023-11-01 20:12:52 +0000

    Wed, Nov 1, 2023, 1:12 PM -07:00

--- Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
+++ Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
@@ -56,6 +56,11 @@ requested_toppings = ["mushrooms", "extra cheese"]
 
 if "mushrooms" in requested_toppings:
     print("Adding mushrooms.")
-    
+elif "pepperoni" in requested_toppings:
+    print("Adding pepperoni.")
+elif "extra cheese" in requested_toppings:
+    print("Adding extra cheese")
+
+print("Finished making your pizza!")
 
 
