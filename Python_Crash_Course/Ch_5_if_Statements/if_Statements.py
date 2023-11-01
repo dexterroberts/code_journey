fc45150b5434288c089e73694031945015e566e9
Dexter <112994984+dexterroberts@users.noreply.github.com>
2023-11-01 21:46:20 +0000

    Wed, Nov 1, 2023, 2:46 PM -07:00

--- Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
+++ Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
@@ -141,3 +141,11 @@
 
 ## Checking for Special Items
 
+requested_toppings = ["mushrooms", "green peppers", "extra cheese"]
+
+for requested_topping in requested_toppings:
+    print(f"Adding {requested_topping}.")
+
+print("\nFinished making your pizza!")
+
+for 
