eec17f1756e204ae0510b0a33c0d9113759567e9
Dexter <112994984+dexterroberts@users.noreply.github.com>
2023-11-01 21:58:28 +0000

    Wed, Nov 1, 2023, 2:58 PM -07:00

--- Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
+++ Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
@@ -170,4 +170,12 @@
 
 available_toppings = ["mushrooms", "olives", "green peppers", "pepperoni", "pineapple", "extra cheese"]
 
-requested_toppings = ["mushroom", ]
+requested_toppings = ["mushroom", "french_fries", "extra cheese"]
+
+for requested_topping in requested_topping:
+    if requested_topping in available_toppings:
+        print(f"Adding "{requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+
+print("Finished making your pizza!")
