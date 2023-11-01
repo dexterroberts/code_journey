d451fb814d15660f69affcdeb4071c5e3c68cb92
Dexter <112994984+dexterroberts@users.noreply.github.com>
2023-11-01 21:50:42 +0000

    Wed, Nov 1, 2023, 2:50 PM -07:00

--- Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
+++ Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
@@ -141,16 +141,18 @@
 
 ## Checking for Special Items
 
-requested_toppings = ["mushrooms", "green peppers", "extra cheese"]
+#requested_toppings = ["mushrooms", "green peppers", "extra cheese"]
 
 #for requested_topping in requested_toppings:
 #    print(f"Adding {requested_topping}.")
 
 #print("\nFinished making your pizza!")
-for requested_topping in requested_toppings:
-    if requested_topping == "green peppers":
-        print("Sorry, we are out of green peppers right now.")
-    else:
-        print(f"Adding {requested_topping}.")
+#for requested_topping in requested_toppings:
+#    if requested_topping == "green peppers":
+#        print("Sorry, we are out of green peppers right now.")
+#    else:
+#        print(f"Adding {requested_topping}.")
 
-print("\nFinished making your pizza!")
+#print("\nFinished making your pizza!")
+
+## Checking That a List Is Not Empty
