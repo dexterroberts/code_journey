b4323c1b824a1243f492b2c5ef0305e21cf0a385
Dexter <112994984+dexterroberts@users.noreply.github.com>
2023-11-01 17:20:47 +0000

    Wed, Nov 1, 2023, 10:20 AM -07:00

---
+++ Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
@@ -0,0 +1,10 @@
+### Date Created: 11/1/23 ###
+### Chapter 5: if Statements ###
+
+## if Statements
+
+# Simple if Statements: The simplest kind of if statement has one test and one action:
+
+# if conditional_test:
+    # do something
+
