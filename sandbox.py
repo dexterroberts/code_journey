8d6d8cf68c8204a8c11a4505270a0ccb0af747ac
Dexter <112994984+dexterroberts@users.noreply.github.com>
2023-11-01 07:19:20 +0000

    Wed, Nov 1, 2023, 12:19 AM -07:00

--- sandbox.py
+++ sandbox.py
@@ -3,12 +3,12 @@
 
 # Create a list of cars
 cars = ["bmw", "honda", "audi", "mercedes", "toyota"]
-for car in cars:
-    if car == "bmw":
-        print(car.upper())
-    else:
-        print(car.title())
+#for car in cars:
+#    if car == "bmw":
+#        print(car.upper())
+#    else:
+#        print(car.title())
 
 # sort cars in alphabetical order
 cars.sort()
-print(cars).title()
+print(f"{cars.title()}")
