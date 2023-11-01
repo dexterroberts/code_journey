7db326be293df8b2c042f8af99f3b3c3eaa928b8
Dexter <112994984+dexterroberts@users.noreply.github.com>
2023-11-01 20:50:37 +0000

    Wed, Nov 1, 2023, 1:50 PM -07:00

--- Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
+++ Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
@@ -113,6 +113,18 @@
 #    print("You just earned 15 points.\n")
 
 # 5-6 Stages of Life
-age = 2
+age = 5
 
+if age < 2:
+    print("You're a baby!")
+elif age >= 2 and age < 4:
+    print("You're a toddler!")
+elif age >= 4 and age < 13:
+    print("You're a kid!")
+elif age >= 13 and age < 20:
+    print("You're a teenager!")
+elif age >= 20 and age < 65:
+    print("You're an adult!")
+else:
+    print("You're an elder!")
 
