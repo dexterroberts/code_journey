019781adc8cb01781f68ca5e76288dba9504acea
Dexter <112994984+dexterroberts@users.noreply.github.com>
2023-11-01 20:26:58 +0000

    Wed, Nov 1, 2023, 1:26 PM -07:00

--- Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
+++ Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
@@ -76,7 +76,8 @@
 
 # 5-4 Alien Colors #2
 alien_color = "green"
-if alien_color == "green":
-    print("You just earned 5 points!")
-else:
-    print("You just earned 10 points!")
+for alien in alien_color:
+    if alien == "green":
+        print("You just earned 5 points!")
+    else:
+        print("You just earned 10 points!")
