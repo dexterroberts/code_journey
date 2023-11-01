2e6457107fb61da80453b9d3198da8a429681f52
Dexter <112994984+dexterroberts@users.noreply.github.com>
2023-11-01 17:28:59 +0000

    Wed, Nov 1, 2023, 10:28 AM -07:00

--- Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
+++ Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
@@ -8,8 +8,17 @@
 # if conditional_test:
     # do something
 
-# EXAMPLE 1#
-age = 19
+# EXAMPLE 1 #
+#age = 19
+#if age >= 18:
+#    print("You are old enough to vote!")
+#    print("Have you registered to vote yet?")
+
+# if-else Statements
+
+# EXAMPLE 2 #
+age = 17
 if age >= 18:
     print("You are old enough to vote!")
-    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.\nPlease register to vote as soon as you turn 18!")
