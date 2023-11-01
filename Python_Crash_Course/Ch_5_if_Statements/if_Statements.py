d2e483b10ba2fb920c148458225e8fe51b65ce3d
Dexter <112994984+dexterroberts@users.noreply.github.com>
2023-11-01 17:43:09 +0000

    Wed, Nov 1, 2023, 10:43 AM -07:00

--- Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
+++ Python_Crash_Course/Ch_5_if_Statements/if_Statements.py
@@ -38,13 +38,18 @@
 #    print("Your admission cost is $40.")
 
 # EXAMPLE 3a #
-age = 12
+#age = 12
 
-if age < 4:
-    price = 0
-elif age < 18:
-    price = 25
-else:
-    price = 40
+#if age < 4:
+#    price = 0
+#elif age < 18:
+#    price = 25
+#else:
+#    price = 40
 
-print(f"Your admission cost is ${price}.")
+#print(f"Your admission cost is ${price}.")
+
+
+## Using Multiple elif Blocks
+
+
