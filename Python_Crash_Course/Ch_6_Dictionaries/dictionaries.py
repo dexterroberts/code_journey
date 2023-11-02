ab05bede82c4d8377daaed3af122a03daf32def2
dj <112994984+dexterroberts@users.noreply.github.com>
2023-11-02 08:58:43 -0700

    Update dictionaries.py

--- Python_Crash_Course/Ch_6_Dictionaries/dictionaries.py
+++ Python_Crash_Course/Ch_6_Dictionaries/dictionaries.py
@@ -1,4 +1,33 @@
 ### Date Created: 11/01/23
 ### Chapter 6: Dictionaries
 
+## A Simple Dictionary ## 
 
+#alien_0 = {"color": "green", "points": 5}
+
+#print(alien_0["color"])
+#print(alien_0["points"])
+
+## Working with Dictionaries ##
+
+#alien_0 = {"color": "green", "points": 5}
+
+#new_points = alien_0["points"]
+#print(f"You just earned {new_points} points!")
+
+# Adding New Key-Value Pairs #
+#alien_0 = {"color": "green", "points": 5}
+#print(alien_0)
+
+#alien_0["x_position"] = 0
+#alien_0["y_position"] = 25
+#print(alien_0)
+
+## Starting with an Empty Dictionary ##
+
+alien_0 = {}
+
+alien_0["color"] = "green"
+alien_0["points"] = 5
+
+print(alien_0)
