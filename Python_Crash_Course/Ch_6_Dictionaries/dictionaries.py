d5c3bd7ccbabb184168afce4ba2e45e7c7dc3f6f
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-02 22:19:44 -0700

    modified file

--- Python_Crash_Course/Ch_6_Dictionaries/dictionaries.py
+++ Python_Crash_Course/Ch_6_Dictionaries/dictionaries.py
@@ -33,20 +33,33 @@
 #print(alien_0)
 
 ## Modifiying Values in a Dictionary
-alien_0 = {"x_position": 0, "y_position": 25, "speed": "medium"}
-print(f"Original position: {alien_0["x_position"]}")
+#alien_0 = {"x_position": 0, "y_position": 25, "speed": "medium"}
+#print(f"Original position: {alien_0["x_position"]}")
 
 # Move the alien to the right
 # Determine how far to move the alien based on its current speed.
-if alien_0["speed"] == "slow":
-    x_increment = 1
-elif alien_0["speed"] == "medium":
-    x_increment = 2
-else:
+#if alien_0["speed"] == "slow":
+#    x_increment = 1
+#elif alien_0["speed"] == "medium":
+#    x_increment = 2
+#else:
     # This must be a fast alien.
-    x_increment = 3
+#    x_increment = 3
 
 # The new position is the old position plus the increment.
-alien_0["x_position"] = alien_0["x_position"] + x_increment
+#alien_0["x_position"] = alien_0["x_position"] + x_increment
 
-print(f"New position: {alien_0["x_position"]}")
+#print(f"New position: {alien_0["x_position"]}")
+
+
+## Removing Key-Value Pairs
+#alien_0 = {"color": "green", "points": 5}
+#print(alien_0)
+
+#del alien_0["points"]
+#print(alien_0)
+
+## A Dictionary of Similar Objects
+
+favorite_languages = {"jen": "python", "sarah": "c", "edward": "rust", "phil": "python"}
+print(favorite_languages)
