2449ffef771c1cc57703d4072f93f7e8515793c8
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-02 22:16:59 -0700

    modified file

--- Python_Crash_Course/Ch_6_Dictionaries/dictionaries.py
+++ Python_Crash_Course/Ch_6_Dictionaries/dictionaries.py
@@ -25,9 +25,28 @@
 
 ## Starting with an Empty Dictionary ##
 
-alien_0 = {}
+#alien_0 = {}
 
-alien_0["color"] = "green"
-alien_0["points"] = 5
+#alien_0["color"] = "green"
+#alien_0["points"] = 5
 
-print(alien_0)
+#print(alien_0)
+
+## Modifiying Values in a Dictionary
+alien_0 = {"x_position": 0, "y_position": 25, "speed": "medium"}
+print(f"Original position: {alien_0["x_position"]}")
+
+# Move the alien to the right
+# Determine how far to move the alien based on its current speed.
+if alien_0["speed"] == "slow":
+    x_increment = 1
+elif alien_0["speed"] == "medium":
+    x_increment = 2
+else:
+    # This must be a fast alien.
+    x_increment = 3
+
+# The new position is the old position plus the increment.
+alien_0["x_position"] = alien_0["x_position"] + x_increment
+
+print(f"New position: {alien_0["x_position"]}")
