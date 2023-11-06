3468082aef7205a7c7c00fe9a41d4e7f94903721
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-05 22:01:01 -0800

    saving progress

--- Python_Crash_Course/Ch_6_Dictionaries/dictionaries.py
+++ Python_Crash_Course/Ch_6_Dictionaries/dictionaries.py
@@ -276,3 +276,16 @@
 
 # A List in a Dictionary
 
+# Store information abouta pizza being ordered
+#pizza = {
+#    "crust": "thick",
+#    "toppings": ["mushrooms", "extra cheese"],
+#    }
+
+# Summarize the order
+#print(f"You ordered a {pizza["crust"]}-crust pizza "
+#      "with the following toppings:")
+
+#for topping in pizza["toppings"]:
+#    print(f"\t{topping}")
+
