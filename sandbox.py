ad85dad160952eeecefbd14a1491552a9099a423
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-08 00:14:39 -0800

    setting up player classes. warrior class created

--- sandbox.py
+++ sandbox.py
@@ -1,10 +1,18 @@
 ## PRACTICE ##
 
-# create a text based game that has decision trees
+# Create a text based game that has decision trees
 
-# welcome message
+# Welcome message
 username = input("Welcome to the text-based game, 'Sandbox!'\n"
       "To start, please enter your first name: ")
 
-print(f"Hi {username}, let's get started!")
+print(f"Hi {username}, let's get started!"\n\n)
+
+# Have user choose from 3 player classes
+player_class_warrior = {
+    "description" = "The Warrior class has sufficient average HEALTH POINTS (HP) and high ATTACK (ATK) points but slightly lower DEFENSE (DEF) points.",
+    "hp": "300",
+    "atk": "500",
+    "def": "200", 
+}
 
