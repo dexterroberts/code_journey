a6bf3b8c78e0d2931d02f3e85f89966454a11e40
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-08 00:09:44 -0800

    welcome message & username input

--- sandbox.py
+++ sandbox.py
@@ -3,3 +3,8 @@
 # create a text based game that has decision trees
 
 # welcome message
+username = input("Welcome to the text-based game, 'Sandbox!'\n"
+      "To start, please enter your first name: ")
+
+print(f"Hi {username}, let's get started!")
+
