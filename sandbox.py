2590bbe89f028ce2dc6209d42f917b95867ac82c
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-08 00:06:02 -0800

    short text based game for practice writing code

--- sandbox.py
+++ sandbox.py
@@ -1,30 +1,5 @@
 ## PRACTICE ##
 
-# create a dictionary
+# create a text based game that has decision trees
 
-user = {
-    "first_name": "jack",
-    "last_name": "robinson",
-    "username": "jrob",
-    "hobbies": ["baseball", "hiking", "gym"]
-    }
-
-user_1 = {
-    "first name": "sara",
-    "last_name": "taylor",
-    "username": "saratay",
-    "hobbies": ["photography", "baking", "hiking"]
-    }
-
-users = [user, user_1]
-
-unique_hobbies = set()
-
-for user_data in users:
-    unique_hobbies.update(user_data["hobbies"])
-
-unique_hobbies_list = list(unique_hobbies)
-
-formatted_hobbies = ", ".join(unique_hobbies_list)
-
-print(f"Unique Hobbies: {formatted_hobbies.title()}")
+# welcome message
