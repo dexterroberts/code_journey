51a9c4eb3ef9aece0b407d2cce5374835cd55d92
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-08 00:22:15 -0800

    minor fixes

--- sandbox.py
+++ sandbox.py
@@ -6,31 +6,30 @@
 username = input("Welcome to the text-based game, 'Sandbox!'\n"
       "To start, please enter your first name: ")
 
-print(f"Hi {username}, let's get started!"\n\n)
+print(f"Hi {username}, let's get started!\n\n")
 
 # Have user choose from 3 player classes
 knight = {
-    "description" = "Strong defender, balanced fighter with good health.",
+    "description": "Strong defender, balanced fighter with good health.",
     "hp": "400",
     "atk": "250",
     "def": "350", 
-}
+    }
 
 rogue = {
-    "description" = "Agile, high attacker with lower defense but moderate health.",
+    "description": "Agile, high attacker with lower defense but moderate health.",
     "hp": "400",
     "atk": "250",
     "def": "350",
-}
+    }
 
 wizard = {
-    "description" = "Wizard: Powerful spellcaster, fragile, but deals massive damage with spells.",
+    "description": "Wizard: Powerful spellcaster, fragile, but deals massive damage with spells.",
     "hp": "400",
     "atk": "250",
     "def": "350",
-}
+    }
 
 player_classes = [knight, rogue, wizard]
 
-print(player_classes)
 
