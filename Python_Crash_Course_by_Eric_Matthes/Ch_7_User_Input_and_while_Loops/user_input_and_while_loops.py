6f0197a27b157aab8c8c5c8a17b55ebea104b948
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-10 11:33:18 -0800

    test example code

--- Python_Crash_Course_by_Eric_Matthes/Ch_7_User_Input_and_while_Loops/user_input_and_while_loops.py
+++ Python_Crash_Course_by_Eric_Matthes/Ch_7_User_Input_and_while_Loops/user_input_and_while_loops.py
@@ -2,7 +2,16 @@
 ### Chapter 7: User Input and while Loops
 
 ## Introducing while Loops
-current_number = 1
-while current_number <= 5:
-    print(current_number)
-    current_number += 1
+#current_number = 1
+#while current_number <= 5:
+#    print(current_number)
+#    current_number += 1
+
+# Letting the User Choose When to Quit
+prompt = "\nTell me something, and I will repeat it back to you."
+prompt += "\nEnter 'quit to end the program."
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    print(message)
