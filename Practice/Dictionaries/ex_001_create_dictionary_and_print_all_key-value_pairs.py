10de973719862c51cf2eadabfc4f6ea961ce88e1
dj <112994984+dexterroberts@users.noreply.github.com>
2023-11-13 11:33:26 -0800

    update exercise

--- Practice/Dictionaries/ex_001_create_dictionary_and_print_all_key-value_pairs.py
+++ Practice/Dictionaries/ex_001_create_dictionary_and_print_all_key-value_pairs.py
@@ -1,2 +1,13 @@
 # Date Created: 11/13/23
 
+# Create a dictionary representing a person with keys 'name', 'age', and 'city
+# Print each key-value pair.
+
+person = {
+    "name": "jake",
+    "age": 25,
+    "city": "dallas",
+    }
+
+for key, value in person.items():
+    print(key, value)
