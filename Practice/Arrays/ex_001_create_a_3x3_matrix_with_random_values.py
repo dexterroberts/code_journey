cdd6d518db651c8e07696e7e1fea074d2e357acf
dj <112994984+dexterroberts@users.noreply.github.com>
2023-11-13 12:01:45 -0800

    added practice examples

--- Practice/Arrays/ex_001_create_a_3x3_matrix_with_random_values.py
+++ Practice/Arrays/ex_001_create_a_3x3_matrix_with_random_values.py
@@ -5,8 +5,18 @@
 import numpy as np
 
 # Create a 3x3 matrix
-matrix = np.array([[1, 2, 3]
+matrix = np.array([[1, 2, 3],
                    [4, 5, 6],
                    [7, 8, 9]])
 
-print(matrix)
+#print(matrix)
+
+## MISC PRACTICE ##
+
+matrix_1 = np.array([[2, 4, 6],
+                     [8, 10, 12],
+                     [14, 16, 18]])
+
+combined = matrix + matrix_1
+
+print(combined)
