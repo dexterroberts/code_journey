0dc0e2c053e62e91dfd886583b22c68eb460497b
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-13 12:37:33 -0800

    removed duplicated code

--- Practice/Arrays/ex_002_sum_of_array_elements.py
+++ Practice/Arrays/ex_002_sum_of_array_elements.py
@@ -10,14 +10,3 @@ sum_of_elements = sum(num_list)
 # Print the result
 print("The sum of all elements in the array is:", sum_of_elements)
 
-### Date Created: 11/13/23
-
-## Write a program that calculates and prints the sum of all elements in an array.
-
-num_list = [1, 5, 13, 7, 24, 40]
-
-# Calculate the sum of all elements in the list
-sum_of_elements = sum(num_list)
-
-# Print the result
-print("The sum of all elements in the array is:", sum_of_elements)
