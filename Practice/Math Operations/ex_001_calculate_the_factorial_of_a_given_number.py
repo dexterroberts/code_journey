8ce6c0b781c739f6aef73b717cce314964fe0143
dj <112994984+dexterroberts@users.noreply.github.com>
2023-11-13 12:06:37 -0800

    completed exercise

--- Practice/Math Operations/ex_001_calculate_the_factorial_of_a_given_number.py
+++ Practice/Math Operations/ex_001_calculate_the_factorial_of_a_given_number.py
@@ -2,3 +2,18 @@
 
 ## Write a program to calculate the factorial of given number
 
+# Function to calculate the factorial of a number
+def factorial(n):
+    # Base case: factorial of 0 or 1 is 1
+    if n == 0 or n == 1:
+        return 1
+    else:
+        # Recursive case: n! = n * (n-1)!
+        return n * factorial(n-1)
+
+# Test the function with a specific number
+number = 5
+result = factorial(number)
+
+# Print the result
+print(f"The factorial of {number} is: {result}")
