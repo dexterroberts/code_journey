271535329e1ed07fb3dc552ca1fa2ec90258714f
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-12 18:40:21 -0800

    Try It Yourself example 7-4

--- Python_Crash_Course_by_Eric_Matthes/Ch_7_User_Input_and_while_Loops/user_input_and_while_loops.py
+++ Python_Crash_Course_by_Eric_Matthes/Ch_7_User_Input_and_while_Loops/user_input_and_while_loops.py
@@ -40,4 +40,7 @@
 
 #    print(current_number)
 
-# Avoiding Infinite Loops
+# TRY IT YOURSELF #
+# 7-4 Pizza Toppings: Write a loop that prompts the user to enter a series of pizza toppings until they
+# enter a 'quit' value. As they enter each topping, print a message saying you'll add that topping to their pizza.
+
