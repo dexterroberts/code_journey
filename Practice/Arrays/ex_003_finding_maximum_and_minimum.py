90c6d07a554ebab84c58b1320dafc71366971263
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-13 12:42:12 -0800

    added f string

---
+++ Practice/Arrays/ex_003_finding_maximum_and_minimum.py
@@ -0,0 +1,9 @@
+### Date Created: 11/13/23
+
+## Create an array of numbers and write a program to find the maximum and minimum values in the array
+
+num_list = [13, 7, 20, 5, 11, 3, 27, 12, 45, 34, 4, 33, 18]
+min_num_list = min(num_list)
+max_num_list = max(num_list)
+
+print(f"Minimum number:{min_num_list}\nMaximum number:{max_num_list}")
