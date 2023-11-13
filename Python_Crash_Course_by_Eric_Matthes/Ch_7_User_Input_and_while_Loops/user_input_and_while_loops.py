4802917d03b88118d9cc16102a99718952b0647d
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-12 18:34:56 -0800

    using break to exit a loop

--- Python_Crash_Course_by_Eric_Matthes/Ch_7_User_Input_and_while_Loops/user_input_and_while_loops.py
+++ Python_Crash_Course_by_Eric_Matthes/Ch_7_User_Input_and_while_Loops/user_input_and_while_loops.py
@@ -15,6 +15,17 @@
 #while message != 'quit':
 #    message = input(prompt)
 
-if message == 'quit':
-    print("\nThanks for playing!")
+#if message == 'quit':
+#    print("\nThanks for playing!")
+
+# Using break to Exit a Loop
+#prompt = "\nPlease enter the name of a city you have visited: "
+#prompt += "\n(Enter 'quit' when you are finished). "
+
+#while True:
+#    city = input(prompt)
+
+#    if city == "quit":
+#        break
+#    else: print(f"I'd love to go to {city.title()}!")
 
