1100c9d3c5340720bfe6b6a4af98fe3a2472d458
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-13 11:42:12 -0800

    added comments and capitalized values

--- Practice/Dictionaries/ex_001_create_dictionary_and_print_all_key-value_pairs.py
+++ Practice/Dictionaries/ex_001_create_dictionary_and_print_all_key-value_pairs.py
@@ -10,4 +10,10 @@ person = {
     }
 
 for key, value in person.items():
-    print(f"{key.title()}: {value.title()}")
+    # Check if the value is a string before applying title()
+    if isinstance(value, str):
+        # Print key and capitalized value if it's a string
+        print(f"{key.title()}: {value.title()}")
+    else:
+        # Print key and value as is if it's not a string (e.g., for integers)
+        print(f"{key.title()}: {value}")
