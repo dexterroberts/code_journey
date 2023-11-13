a1882f1244ea3014807a473ef83941a41387eb5b
dj <112994984+dexterroberts@users.noreply.github.com>
2023-11-13 12:26:13 -0800

    new exercise started

---
+++ Practice/Arrays/ex_002_sum_of_array_elements.py
@@ -0,0 +1,9 @@
+### Date Created: 11/13/23
+
+## Write a program that calculates and prints the sum of all elements in an array.
+
+num_list = [1, 5, 13, 7, 24, 40]
+
+for num in num_list:
+    num = sum[0:]
+    print(num)
