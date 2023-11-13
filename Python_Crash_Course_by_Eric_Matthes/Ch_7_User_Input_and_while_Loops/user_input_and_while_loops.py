e4a8c4392622da8fe98bc28af94e6d35305d91af
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-12 19:31:01 -0800

    completed 7-6; versions already satisfied

--- Python_Crash_Course_by_Eric_Matthes/Ch_7_User_Input_and_while_Loops/user_input_and_while_loops.py
+++ Python_Crash_Course_by_Eric_Matthes/Ch_7_User_Input_and_while_Loops/user_input_and_while_loops.py
@@ -81,4 +81,9 @@
 #    else:
 #        print("Your ticket is $15.")
 
-# 7-6 Three Exits
+# 7-6 Three Exits: Write different version of either Exercise 7-4 or 7-5 that do each of the following at least once:
+    # - Use a conditional test in the while statement to stop the loop
+    # - Use an active variable to control how long the loop runs
+    # - Use a break statement to exit the loop when the user enters a 'quit' value
+
+
