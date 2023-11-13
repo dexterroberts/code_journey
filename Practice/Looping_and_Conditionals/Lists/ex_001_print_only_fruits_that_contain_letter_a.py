114dbe65cb8c7146b3456e53a84ca1c772944fa1
dj <112994984+dexterroberts@users.noreply.github.com>
2023-11-13 11:22:30 -0800

    update exercise

---
+++ Practice/Looping_and_Conditionals/Lists/ex_001_print_only_fruits_that_contain_letter_a.py
@@ -0,0 +1,10 @@
+### Date Created: 11/13/23
+
+## Create a list of fruits and write a program to print only the fruits that contain the letter 'a'
+
+# Create a list of fruits
+fruits = ["apple", "banana", "cherry", "lime", "honeydew", "kiwi", "watermelon"]
+
+for fruit in fruits:
+    if "a" in fruit:
+        print(fruit)
