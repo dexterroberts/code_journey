f4a391b895605aa815e649049745aa934d736d1b
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-12 18:29:51 -0800

    beginning examples for Using break to exit a Loop

--- Python_Crash_Course_by_Eric_Matthes/Ch_7_User_Input_and_while_Loops/user_input_and_while_loops.py
+++ Python_Crash_Course_by_Eric_Matthes/Ch_7_User_Input_and_while_Loops/user_input_and_while_loops.py
@@ -8,13 +8,37 @@
 #    current_number += 1
 
 # Letting the User Choose When to Quit
-prompt = "\nTell me something, and I will repeat it back to you."
-prompt += "\nEnter 'quit to end the program.' "
+#prompt = "\nTell me something, and I will repeat it back to you."
+#prompt += "\nEnter 'quit to end the program.' "
 
-message = ""
-while message != 'quit':
-    message = input(prompt)
+#message = ""
+#while message != 'quit':
+#    message = input(prompt)
 
-if message == 'quit':
-    print("\nThanks for playing!")
+#if message == 'quit':
+#    print("\nThanks for playing!")
 
+# Using a Flag
+#prompt = "\nTell me something, and I will repeat it back to you: "
+#prompt += "\nEnter 'quit' to end the program. "
+
+#active = True
+#while active:
+#    message = input(prompt)
+
+#    if message == 'quit':
+#        active = False
+#    else:
+#        print(message)
+
+# Using break to Exit a Loop
+prompt = "\nPlease enter the name of a city you have visited: "
+prompt += "\nEnter 'quit' when you are finished. "
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
