f46efb443a803357af134028c758e57d32dd5b8a
dj <112994984+dexterroberts@users.noreply.github.com>
2023-11-13 11:51:15 -0800

    created folders and started array exercise

---
+++ Practice/Arrays/ex_001_create_a_3x3_matrix_with_random_values.py
@@ -0,0 +1,12 @@
+### Date Created: 11/13/23
+
+## Create a 3x3 matrix with random values. Then, print the transpose of the matrix.
+
+import numpy as np
+
+# Create a 3x3 matrix
+matrix = np.array([[1, 2, 3]
+                   [4, 5, 6],
+                   [7, 8, 9]])
+
+print(matrix)
