ee2e33bf6d0ea914a9db188f40bb50c9aad144c3
dexterroberts <dexterrobertsjr@gmail.com>
2023-11-13 11:09:10 -0800

    complete

--- Practice/Looping_and_Conditionals/exercise_001.py
+++ Practice/Looping_and_Conditionals/exercise_001.py
@@ -2,9 +2,6 @@
 
 ## Write a Python program that prints all the even numbers from 1 to 20 using a for loop.
 
-# Establish starting point number
-x = 1
-
-for num in x < 21:
+for num in range(1, 21):
     if num % 2 == 0:
         print(num)
